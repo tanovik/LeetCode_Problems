4df20b9597353a154d65ada8a16b0982a64cb3c6
tanovik <74468593+tanovik@users.noreply.github.com>
2024-05-03 08:45:24 +0300

    Time: 68 ms (62.67%), Space: 52.7 MB (82.61%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,15 @@
+function removeDuplicates(nums: number[]): number {
+ let k = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < nums[i + 1]) {
+      nums[k] = nums[i]
+      k++
+    } else if (nums[i + 1] === undefined) {
+      nums[k] = nums[i]
+      k++
+    }
+  }
+
+  return k
+};
