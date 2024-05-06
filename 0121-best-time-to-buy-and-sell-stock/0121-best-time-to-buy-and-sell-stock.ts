77160d8cd253de2c743c8daded5fae442f79497b
tanovik <74468593+tanovik@users.noreply.github.com>
2024-05-06 18:01:08 +0300

    Time: 76 ms (71.08%), Space: 59.6 MB (66.83%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,12 @@
+function maxProfit(prices: number[]): number {
+    let profit = 0
+    let min = prices[0]
+    for(let i = 1; i < prices.length; i++){
+        // The day we should buy at
+        min = Math.min(min, prices[i-1])
+        // Check if selling at the current day gives us the most profit
+        profit = Math.max(prices[i]-min, profit)
+    }
+    return profit
+};
+
