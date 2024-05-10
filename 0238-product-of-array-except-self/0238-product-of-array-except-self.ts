d082518be95d5afe5ffddd5b45c8400883c6c704
tanovik <74468593+tanovik@users.noreply.github.com>
2024-05-10 12:45:27 +0300

    Time: 109 ms (33.98%), Space: 66.7 MB (25.33%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -0,0 +1,17 @@
+function productExceptSelf(nums: number[]): number[] {
+    const answer = new Array(nums.length).fill(1)
+
+    let product=1
+    for(let i=0;i<nums.length; i++){
+        answer[i]*=product
+        product*=nums[i]
+    }
+
+    product=1
+    for(let i=nums.length-1;i>=0; i--){
+       
+        answer[i]*=product
+        product*=nums[i]
+    }
+    return answer
+};
