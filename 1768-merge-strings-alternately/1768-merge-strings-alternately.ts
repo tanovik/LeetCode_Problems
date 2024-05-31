369453acba8f273e1ac03fe0fbce889e125462b2
tanovik <74468593+tanovik@users.noreply.github.com>
2024-05-31 20:13:47 +0300

    Time: 61 ms (42.99%), Space: 52.4 MB (25.42%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
@@ -0,0 +1,17 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let mergedString: string = '';
+    const length = Math.max(word1.length,word2.length)
+
+    for(let i=0; i<length; i++){
+        if(word1[i]){
+            mergedString += word1[i]
+        }
+
+        if(word2[i]){
+            mergedString += word2[i]
+        }
+    }
+
+
+    return mergedString
+};
