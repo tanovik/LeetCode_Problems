5a23f686e47d61f8505f0e60c0f3306c6f1428a3
tanovik <74468593+tanovik@users.noreply.github.com>
2024-07-01 19:20:04 +0300

    Time: 69 ms (29.02%), Space: 52.3 MB (60.65%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -0,0 +1,18 @@
+function isValid(s: string): boolean {
+
+    let stack: string[] = [];
+
+    for(let i = 0; i< s.length; i++){
+       if(s[i] === '('   || s[i] === '{' || s[i] === '['){
+           stack.push(s[i]);
+       }else{
+           const top = stack.pop();
+
+           if(s[i] === ')' && top !== '(' || s[i] === '}' && top !== '{'  || s[i] === ']' && top !== '[') {
+               return false;
+           }
+       }
+    }
+    return stack.length === 0;
+
+};
