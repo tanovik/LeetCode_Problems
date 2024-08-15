fc4c677f4ed002ea7aae8311a052bc6653e377a9
tanovik <74468593+tanovik@users.noreply.github.com>
2024-08-15 22:08:12 +0300

    Time: 70 ms (28.41%), Space: 53.9 MB (38.73%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    if (root === null) return 0
+    let lDepth = maxDepth(root.left);
+    let rDepth = maxDepth(root.right);
+     return (lDepth > rDepth)? (lDepth+1) : (rDepth+1)
+};
